24fbc8890f5fff25a7075be84d17b9b9bf17f474
chipdelmal <chipdelmal@gmail.com>
2022-11-02 11:03:24 -0700

    CEF Functional GUI

--- PAN/CEF/app/app.py
+++ PAN/CEF/app/app.py
@@ -56,32 +56,32 @@ app.layout = dbc.Container([
                 html.H2("psSIT Cost Effectiveness"),
                 dbc.Col(html.Hr())
             ]),
-            width={'size': 12, 'offset': 0, 'order': 0}
-        ), style = {'textAlign': 'center', 'paddingBottom': '1%'}
+            width={'size':12, 'offset':0, 'order':0}
+        ), style={'textAlign':'center', 'paddingBottom':'1%', 'paddingTop':'2%'}
     ),
     dbc.Row(        
         dbc.Col(
             dbc.Container([
-                lay.ren_div,
-                lay.res_div,
-                lay.rei_div
+                lay.ren_div, lay.res_div, lay.rei_div,
+                dbc.Col(html.Hr()),
+                lay.pct_div, lay.pmd_div,
+                dbc.Col(html.Hr()),
+                lay.mtf_div, 
+                dbc.Col(html.Hr()),
+                lay.fvb_div, lay.mfr_div, 
+                dbc.Col(html.Hr()),
             ]),
-            width={'size': 12, 'offset': 0, 'order': 0}
+            width={'size':'100%', 'offset': 0, 'order': 0}
         ), style = {'textAlign': 'left', 'paddingBottom': '1%'}
     ),
     dbc.Row(        
         dbc.Col(
-            dbc.Container([
-                dbc.Col(html.Hr()),
-                dbc.Col(
-                    html.H2('WOP Prediciton: ')
-                ),
-                dbc.Col(
-                    html.H2(id='wop-out')
-                )
-            ]),
-            width={'size': 12, 'offset': 0, 'order': 0}
-        ), style = {'textAlign': 'center', 'paddingBottom': '1%'}
+            html.Div([
+                html.H2('WOP Prediciton:', style={'display': 'inline-block'}),
+                html.H2(id='wop-out', style={'display':'inline-block', 'margin-left':'10px'})
+            ], style={'display': 'inline-block', 'paddingBottom': '1%'}),
+            width={'size':12, 'offset':0, 'order':0}
+        ), style = {'textAlign':'center', 'paddingBottom':'1%'}
     )
 ])
 
@@ -89,13 +89,18 @@ app.layout = dbc.Container([
     Output(component_id='wop-out', component_property='children'),
     Input('ren-slider', 'value'),
     Input('res-slider', 'value'),
-    Input('rei-slider', 'value')
+    Input('rei-slider', 'value'),
+    Input('pct-slider', 'value'),
+    Input('pmd-slider', 'value'),
+    Input('mfr-slider', 'value'),
+    Input('mtf-slider', 'value'),
+    Input('fvb-slider', 'value')
 )
-def update_prediction(ren, res, rei):
+def update_prediction(ren, res, rei, pct, pmd, mfr, mtf, fvb):
     probe = (
         ('ren', int(ren)), ('rer', int(res)), ('rei', int(rei)),
-        ('pct', 1), ('pmd', 1),
-        ('mfr', 0), ('mtf', 1), ('fvb', 0)
+        ('pct', float(pct)), ('pmd', float(pmd)),
+        ('mfr', float(mfr)), ('mtf', float(mtf)), ('fvb', float(fvb))
     )
     vct = np.array([[i[1] for i in probe]])
     # (prediction, bias, contributions) = ti.predict(RF, vct)
--- PAN/CEF/app/layouts.py
+++ PAN/CEF/app/layouts.py
@@ -10,9 +10,21 @@ def get_marks(start, end, step, norm=None):
     for i in range(start, end, step):
         if norm:
             if i == start or i == end - 1:
-                marks[int(i / norm)] = str(int(i / norm))
+                marks[int(i/norm)] = str(int(i/norm))
             else:
-                marks[i / norm] = str(i / norm)
+                marks[i/norm] = str(i/norm)
+        else:
+            marks[i] = str(i)
+    return marks
+
+def get_marks_float(start, end, step, multiplier=100):
+    marks = dict()
+    (st, en, st) = [
+        int(i*multiplier) for i in (start, end, step)
+    ]
+    for i in range(st, en, st):
+        if i==st or i ==(en-1):
+            marks[int(i)] = str(int(i))
         else:
             marks[i] = str(i)
     return marks
@@ -48,5 +60,56 @@ rei_div = html.Div([
         min=REI[0], max=REI[1],
         step=REI[-2], value=REI[-1],
         marks=get_marks(REI[0], REI[1], REI[-2])
+    )
+])
+###############################################################################
+# Construct Sliders
+###############################################################################
+(PCT, PMD, MFR, MTF, FVB) = [
+    cst.SA_RANGES[i] for i in ('pct', 'pmd', 'mfr', 'mtf', 'fvb')
+]
+pct_div = html.Div([
+    html.H5('Cutting Rate (pct):'),
+    dcc.Slider(
+        id='pct-slider',
+        min=PCT[0], max=PCT[1],
+        step=PCT[-2], value=PCT[-1],
+        marks=get_marks_float(PCT[0], PCT[1], PCT[-2])
+    )
+])
+pmd_div = html.Div([
+    html.H5('Maternal Deposition (pmd):'),
+    dcc.Slider(
+        id='pmd-slider',
+        min=PMD[0], max=PMD[1],
+        step=PMD[-2], value=PMD[-1],
+        marks=get_marks_float(PMD[0], PMD[1], PMD[-2])
+    )
+])
+mfr_div = html.Div([
+    html.H5('Male Fertility (mfr):'),
+    dcc.Slider(
+        id='mfr-slider',
+        min=MFR[0], max=MFR[1],
+        step=MFR[-2], value=MFR[-1],
+        marks=get_marks_float(MFR[0], MFR[1], MFR[-2])
+    )
+])
+mtf_div = html.Div([
+    html.H5('Mating Fitness (mtf):'),
+    dcc.Slider(
+        id='mtf-slider',
+        min=MTF[0], max=MTF[1],
+        step=MTF[-2], value=MTF[-1],
+        marks=get_marks_float(MTF[0], MTF[1], MTF[-2])
+    )
+])
+fvb_div = html.Div([
+    html.H5('Female Viability (fvb):'),
+    dcc.Slider(
+        id='fvb-slider',
+        min=FVB[0], max=FVB[1],
+        step=FVB[-2], value=FVB[-1],
+        marks=get_marks_float(FVB[0], FVB[1], FVB[-2])
     )
 ])
