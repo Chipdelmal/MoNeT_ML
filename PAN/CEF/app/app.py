dd365c1bba2ca90bfe4d805742c80ff9dce0ce2b
chipdelmal <chipdelmal@gmail.com>
2022-11-10 08:33:07 -0800

    CEF Adding legends

--- PAN/CEF/app/app.py
+++ PAN/CEF/app/app.py
@@ -38,65 +38,65 @@ app.layout = html.Div([
             'textAlign':'center',
             'paddingBottom':'1%', 'paddingTop':'1%'
         }
-        # className="bg-primary text-white p-2 mb-2 text-center"
     ),
     dbc.Row(
-        dbc.Col(
-            # html.Div([ 
-            #     html.P("This tool is built for exploration purposes only! For accurate results use MGDrivE!"),
-            #     dbc.Col(html.Hr())
-            # ])
-        ), style={
-            'textAlign':'center',
-            'paddingBottom':'1%', 'paddingTop':'2%'
-        }
+        [
+            dbc.Col(
+                html.Div([
+                    lay.ren_div, lay.res_div, lay.rei_div,
+                    dbc.Col(html.Hr()),
+                    lay.mtf_div, 
+                    dbc.Col(html.Hr()),
+                    lay.fvb_div, lay.mfr_div,
+                    dbc.Col(html.Hr()),
+                    lay.pct_div, lay.pmd_div
+                ]), 
+                width=8,
+                style={'margin-left':'20px'}
+            ),
+            dbc.Col(
+                html.Div([
+                    dbc.Row([
+                        html.H4("Window of Protection", style={'textAlign':'center'}),
+                        html.H6("(R²: 0.92, MAE: 0.05, RMSE: 0.12)", style={'textAlign':'center', 'font-size': '10px'})
+                    ]),
+                    dbc.Row([
+                        dbc.Col(html.Div(lay.wop_gauge)),
+                        # dbc.Col(html.Div(lay.tti_gauge)), 
+                    ]),
+                    dbc.Row([
+                        html.H4("Cumulative Potential for Transmission", style={'textAlign':'center'}),
+                        html.H6("(R²: 0.94, MAE: 0.04, RMSE: 0.11)", style={'textAlign':'center', 'font-size': '10px'})
+                    ]),
+                    dbc.Row([
+                        dbc.Col(html.Div(lay.cpt_gauge)),
+                    ])
+                ]), 
+                width=3,
+                style={'margin-left':'3px', 'margin-right':'0px'}
+            )
+        ], style={'paddingTop': '1%'}
     ),
-    dbc.Row([
-        dbc.Col(
-            html.Div([
-                lay.ren_div, lay.res_div, lay.rei_div,
-                dbc.Col(html.Hr()),
-                lay.mtf_div, 
-                dbc.Col(html.Hr()),
-                lay.fvb_div, lay.mfr_div,
-                dbc.Col(html.Hr()),
-                lay.pct_div, lay.pmd_div
-            ]), 
-            width=8,
-            style={'margin-left':'20px'}
-        ),
-        dbc.Col(
-            html.Div([
-                dbc.Row([
-                    html.H4("Window of Protection", style={'textAlign':'center'}),
-                    html.H6("(R²: 0.92, MAE: 0.05, RMSE: 0.12)", style={'textAlign':'center', 'font-size': '10px'})
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div(lay.wop_gauge)),
-                    # dbc.Col(html.Div(lay.tti_gauge)), 
-                ]),
-                dbc.Row([
-                    html.H4("Cumulative Potential for Transmission", style={'textAlign':'center'}),
-                    html.H6("(R²: 0.94, MAE: 0.04, RMSE: 0.11)", style={'textAlign':'center', 'font-size': '10px'})
-                ]),
-                dbc.Row([
-                    dbc.Col(html.Div(lay.cpt_gauge)),
-                    # dbc.Col(html.Div(lay.tto_gauge))
-                ])
-            ]), 
-            width=3,
-            style={'margin-left':'3px'}
-        )
-    ]),
     dbc.Row([
         dbc.Col(
             html.Div([
                 # html.Hr(),
                 # html.Img(src=app.get_asset_url('SAML.png'), style={'width':'100%'}),
                 html.A(
-                    "by Héctor M. Sánchez C.", href='https://chipdelmal.github.io/', 
-                    target="_blank",
-                    style={'color': '#a2d2ff', 'font-size': '15px'}
+                    "Disclaimer: This tool was created for exploration purposes only. For accurate results use",
+                    style={
+                        'color': '#8d99ae', 'font-size': '15px',
+                        'textAlign':'left'
+                    }
+                ),
+                html.A(
+                    "MGDrivE!",
+                    href='https://marshalllab.github.io/MGDrivE/',
+                    target="_blank", 
+                    style={
+                        'color': '#ff006eCC', 'font-size': '15px',
+                        'textAlign':'left', 'paddingLeft': '5px'
+                    }
                 )
             ]), 
             style={
@@ -104,9 +104,45 @@ app.layout = html.Div([
                 'paddingBottom':'0%', 'paddingTop':'0%',
                 'paddingLeft': '2%', 'paddingRight': '2%'
             }
+        ),
+        dbc.Col(
+            html.Div([
+                html.A(
+                    "Dev. @",
+                    style={'color': '#8d99ae', 'font-size': '15px'}
+                ),
+                html.A(
+                    "Marshall Lab", href='https://www.marshalllab.com/', 
+                    target="_blank",
+                    style={
+                        'color': '#a2d2ff', 'font-size': '15px',
+                        'paddingLeft': '8px'
+                    }
+                ),
+                html.A(
+                    "by",
+                    style={
+                        'color': '#8d99ae', 'font-size': '15px',
+                        'paddingLeft': '8px'
+                    }
+                ),
+                html.A(
+                    "Héctor M. Sánchez C.", href='https://chipdelmal.github.io/', 
+                    target="_blank",
+                    style={
+                        'color': '#a2d2ff', 'font-size': '15px',
+                        'paddingLeft': '8px', 'textAlign':'right'
+                    }
+                )
+            ]), 
+            style={
+                'textAlign': 'right',
+                'paddingBottom':'0%', 'paddingTop':'0%',
+                'paddingLeft': '2%', 'paddingRight': '2%'
+            }
         )
     ])
-])
+], style={'width': "99.2%"})
 
 ###############################################################################
 # Callbacks
